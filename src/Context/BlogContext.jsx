0443bc626e123114b47116f10859be2eb9f606ce
IsmailBogac <92868619+IsmailBogac@users.noreply.github.com>
2025-08-04 19:07:48 +0300

    BlogContext import bug fixed

--- src/Context/BlogContext.jsx
+++ src/Context/BlogContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { defaultBlogs } from "../Data";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { db } from "../Authentication/FireBase";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "../Authentication/firebase";
 
 export const BlogContext = createContext();
 
